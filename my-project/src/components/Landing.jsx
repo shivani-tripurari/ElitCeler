a68e6c82956515ec0ce4876e06e929f84e62b3f3
T Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2024-06-26 23:50:38 +0530

    add

--- my-project/src/components/Landing.jsx
+++ my-project/src/components/Landing.jsx
@@ -24,8 +24,8 @@ const Landing = () => {
       <div className="right">
         <div className="blob"></div>
       </div>
-      <div className="hero-text absolute top-20 right-11 md:top-56 md:right-64 ">
-        <h1 className='text-[44px] pr-2 pb-7 mb-11 md:text-7xl'><span className='white'>PRODUCTS THAT <br/>BRIDGE TO </span> <span className='orange'>REALITY</span> </h1>
+      <div className="hero-text absolute top-20 right-11 md:top-48 md:right-64 ">
+        <h1 className='text-[36px] pr-2 pb-7 mb-11 md:text-7xl'><span className='white'>PRODUCTS THAT <br/>BRIDGE TO </span> <span className='orange'>REALITY</span> </h1>
         <p>The future belongs to the generation who believes in dreams.<br/>
          We at ElitCeler Tech perform infinite times of continuous processes<br/>
           of improvements to fulfil those dreams.</p>
