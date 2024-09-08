51b636e3c3fbafde9fca02cf3d3a7cb2e9ef3e1c
T Shivani <96899982+shivani-tripurari@users.noreply.github.com>
2024-09-08 22:00:43 +0530

    add logos

--- my-project/src/components/Company.jsx
+++ my-project/src/components/Company.jsx
@@ -1,5 +1,13 @@
-import yourlogo from "../assets/yourlogo.svg";
-
+// import yourlogo from "../assets/yourlogo.svg";
+import startup from "../assets/18startup.svg";
+import CRC from "../assets/CRC.svg";
+import group from "../assets/Group.svg";
+import health from "../assets/Healthkare.svg";
+import HH from "../assets/HH.svg";
+import kodeblue from "../assets/Kodeblue.svg";
+import Moksh from "../assets/Moksh.svg";
+import MyHome from "../assets/Myhome.svg";
+import PV from "../assets/PV.svg";
 const Company = ({className}) => {
   return (
     <div className={className}>
@@ -9,40 +17,40 @@ const Company = ({className}) => {
           <ul className="flex items-center justify-center flex-1 h-[7.5rem] m-2">
         
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={startup} height={28} width={134} className="m-4"/> 
         </li>
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={CRC} height={28} width={134} className="m-4"/> 
         </li>
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={health} height={28} width={134} className="m-4"/> 
         </li>
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={group} height={28} width={134} className="m-4"/> 
         </li>
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={HH} height={28} width={134} className="m-4"/> 
         </li>
         </ul>
           </div>
           <div>
-          <ul className="flex items-center justify-center flex-1 h-[1.5rem] m-2">
+          <ul className="flex items-center justify-center flex-1 h-[6.5rem] m-2">
         
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={kodeblue} height={28} width={134} className="m-4"/> 
         </li>
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={Moksh} height={28} width={134} className="m-4"/> 
         </li>
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={MyHome} height={28} width={134} className="m-4"/> 
         </li>
         <li>
-            <img src={yourlogo} height={28} width={134} className="m-4"/> 
+            <img src={PV} height={28} width={134} className="m-4"/> 
         </li>
-        <li>
+        {/* <li>
             <img src={yourlogo} height={28} width={134} className="m-4"/> 
-        </li>
+        </li> */}
         </ul>
           </div>
       </div>
